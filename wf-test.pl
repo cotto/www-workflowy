9ffc291f6b9124ccd925bc1d74e4febd0fa710d3
Christoph Otto <christoph_github@mksig.org>
2012-12-12 14:48:06 -0800

    implement logging in

--- wf-test.pl
+++ wf-test.pl
@@ -12,8 +12,48 @@ my $password = 'test';
 my $ua = LWP::UserAgent->new;
 $ua->cookie_jar({});
 
-my $login_req = LWP::Request->new(POST => 'https://workflowy.com/accounts/login');
-$login_req->content_type('application/x-www-form-urlencoded');
-$login_req->content("username=$username&password=$password");
+unless (log_in($ua, $username, $password)) {
+  die "couldn't log in.  sad face.";
+}
 
+
+#my $req = HTTP::Request->new(GET => 'https://workflowy.com/');
+#my $resp = $ua->request($req);
+#say Dumper($resp);
+
+my $req = HTTP::Request->new(GET => 'https://workflowy.com/get_project_tree_data');
+my $resp = $ua->request($req);
+say Dumper($resp);
+
+# 
+  
+
+
+=item log_in($ua, $username, $password)
+
+Log in to a Workflowy account.
+
+=cut
+
+sub log_in {
+  
+  # will return 200 on failed login, 302 on success
+  my $req = HTTP::Request->new(POST => 'https://workflowy.com/accounts/login/');
+  $req->content_type('application/x-www-form-urlencoded');
+  $req->content("username=$username&password=$password");
+  my $resp = $ua->request($req);
+  
+  if ($resp->code == 200) {
+    say "failed to log in";
+    return 0;
+  }
+
+  if ($resp->code == 302) {
+    say "login successful";
+    return 1;
+  }
+
+  say "not sure what happened";
+  say Dumper($resp);
+}
 
